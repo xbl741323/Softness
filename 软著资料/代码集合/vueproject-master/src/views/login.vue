bfc000a5968772f884da8f481eb18610adf9341b
DESKTOP-PCNSM4L\sevenKill <1546766776@qq.com>
2023-08-03 16:04:04 +0800

    提交更新

--- 软著资料/代码集合/vueproject-master/src/views/login.vue
+++ 软著资料/代码集合/vueproject-master/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-wrap">
     <el-form label-position="left" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm login-container">
-      <h3 class="title">根瘤菌条件化固氮评价系统</h3>
+      <h3 class="title">监控视频行为分析系统</h3>
       <el-form-item prop="username">
         <el-input type="text" v-model="ruleForm.username" auto-complete="off" placeholder="请输入账号"></el-input>
       </el-form-item>
