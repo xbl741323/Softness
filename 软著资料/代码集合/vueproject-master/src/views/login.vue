1e482371ede4921fe6012295ef0b80cb7f9e1bce
DESKTOP-PCNSM4L\sevenKill <1546766776@qq.com>
2023-08-08 16:20:46 +0800

    提交更新

--- 软著资料/代码集合/vueproject-master/src/views/login.vue
+++ 软著资料/代码集合/vueproject-master/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-wrap">
     <el-form label-position="left" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm login-container">
-      <h3 class="title">医院负压隔离房间防污扩散系统</h3>
+      <h3 class="title">医美筑数字一体化手术室系统软件</h3>
       <el-form-item prop="username">
         <el-input type="text" v-model="ruleForm.username" auto-complete="off" placeholder="请输入账号"></el-input>
       </el-form-item>
